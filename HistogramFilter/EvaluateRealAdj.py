567158a85b9a7ed887a9b234989c4e47fe56c04e
RiccardoZanaboni <riccardo.zanboni02@universitadipavia.it>
2019-12-02 01:08:26 +0100

    Histogram and 2 new type of config , house1 and house2

--- HistogramFilter/EvaluateRealAdj.py
+++ HistogramFilter/EvaluateRealAdj.py
@@ -2,11 +2,12 @@ import sys
 
 import networkx as nx
 from matplotlib import gridspec
-
+import numpy as np
 import ReadFile
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
+from collections import OrderedDict
 
 """
     Script used to evaluate the output of the histogram filter between different config.json and 
@@ -23,7 +24,7 @@ def open_json(file_name):
 
 
 def set_up(data_config):
-    data = ReadFile.ReadFile(data_config["info"]["input_file_path"] + data_config["info"]["output_file_name"]).df
+    data = ReadFile.ReadFile(data_config["info"]["output_file_path"] + data_config["info"]["output_file_name"]).df
     return data
 
 
@@ -35,6 +36,15 @@ def set_up(data_config):
 def dictionary_room(data_config):
     key = data_config["info"]["room_name"]
     value = data_config["info"]["columns_name"][1:]
+    dictionary = {}
+    for i in range(0, len(key)):
+        dictionary[key[i]] = value[i]
+    return dictionary
+
+
+def dict_hist(data_config):
+    key = data_config["info"]["room_name"]
+    value = data_config["info"]["state_domain"]
     dictionary = {}
     for i in range(0, len(key)):
         dictionary[key[i]] = value[i]
@@ -54,13 +64,27 @@ def found_max(probability_array: pd.Series, data_config):
     return val_max, index_max
 
 
+def color_bar(sim_config, filter_adj):
+    colors = np.empty(len(filter_adj), dtype=object)
+    dict_adj = sim_config["room"]
+    for i,k in enumerate(filter_adj):
+        for adj in dict_adj[k[0]]:
+            if k[1] == adj:
+                colors[i]='green'
+                break
+            else:
+                colors[i]='red'
+    return colors
+
+
 if __name__ == "__main__":
     # if len(sys.argv) < 2:
     #    print("Manca il nome del file json")
     #    sys.exit(1)
-    conf_file = "config.json"
+    folder = "Configurations_file/"
+    conf_file = folder + "config_house1.json"   # file1 correct
     config = open_json(conf_file)
-    conf_real_adj_file = "config_notequals.json"
+    conf_real_adj_file = folder + "config_house1_complementar_notequals.json"  # file2 wrong
     config_error = open_json(conf_real_adj_file)
     df = pd.DataFrame(columns=['Time', 'Efficiency'])
     df_error = pd.DataFrame(columns=['Time', 'Efficiency_error'])
@@ -91,42 +115,60 @@ if __name__ == "__main__":
     G = nx.Graph()
     for k in key_values:
         G.add_node(k)
-
-    adj_dict = {"bel(A)bel(K)": 0, "bel(A)bel(L)": 0, "bel(A)bel(T)": 0, "bel(A)bel(B)": 0, "bel(K)bel(A)": 0,
-                "bel(K)bel(L)": 0, "bel(K)bel(T)": 0, "bel(K)bel(B)": 0, "bel(L)bel(A)": 0, "bel(L)bel(K)": 0,
-                "bel(L)bel(T)": 0, "bel(L)bel(B)": 0, "bel(T)bel(A)": 0, "bel(T)bel(K)": 0, "bel(T)bel(L)": 0,
-                "bel(T)bel(B)": 0, "bel(B)bel(A)": 0, "bel(B)bel(K)": 0, "bel(B)bel(L)": 0, "bel(B)bel(T)": 0
-                }
-    edge_labels = {}
+    adj_dict = {}
+    for i, c in enumerate(config["info"]["columns_name"][1:]):
+        for n, col in enumerate(config["info"]["columns_name"][1:]):
+            if i != n:
+                adj_dict[str(c) + str(col)] = 0
     for i, r in enumerate(rooms_index_error):
         if rooms_index_error[i] != rooms_index_error[i - 1] and i != 0:
             adj_dict[str(rooms_index_error[i - 1]) + str(r)] += 1
             adj_dict[str(r) + str(rooms_index_error[i - 1])] += 1
             G.add_edge(key_values[list(dictionary_rooms.values()).index(str(rooms_index_error[i - 1]))],
                        key_values[list(dictionary_rooms.values()).index(str(r))])
-    adj_dict = {k: 2*v / sum(adj_dict.values(), 0.0) for k, v in adj_dict.items()}  #2* because matrix is triangular
-    # TODO grafici con piu stanze
+    adj_dict = {k: 2 * v / sum(adj_dict.values(), 0.0) for k, v in adj_dict.items()}  # 2* because matrix is triangular
+
+    edge_labels = {}
     for room in G.edges.keys():
         adj_value = '%.3f' % adj_dict[dictionary_rooms[room[0]] + dictionary_rooms[room[1]]]
         edge_labels[(room[0], room[1])] = adj_value
-    df.to_csv(config["info"]["input_file_path"] + config["info"]["output_evaluation"], index=False)
-    df_error.to_csv(config_error["info"]["input_file_path"] + config_error["info"]["output_evaluation"], index=False)
+    df.to_csv(config["info"]["input_file_path"] + "/Output_evaluation/" + config["info"]["output_evaluation"], index=False)
+    df_error.to_csv(config_error["info"]["input_file_path"] + "/Output_evaluation/" + config_error["info"]["output_evaluation"], index=False)
     fig = plt.figure()
     fig.set_size_inches(12, 9)
-    gs = gridspec.GridSpec(2, 2)
-    ax0=plt.subplot(gs[0, 0])  # row 0, col 0
+    plt.tight_layout()
+    gs = gridspec.GridSpec(2, 3)
+    ax0 = plt.subplot(gs[0, 0])  # row 0, col 0
     ax0.set(xlabel='Time', ylabel='Efficiency')
-    df.plot(kind='line', x='Time', y='Efficiency', ax=ax0)
-    df_error.plot(kind='line', x='Time', y='Efficiency_error', ax=ax0)
+    df.plot(kind='line', x='Time', y='Efficiency', ax=ax0, color="lightcoral")
+    df_error.plot(kind='line', x='Time', y='Efficiency_error', ax=ax0, linestyle='--', dashes=(8, 5), color="blue")
+
     ax1 = plt.subplot(gs[0, 1])  # row 0, col 1
     ax1.set_ylim([-1, 1])
     ax1.set(xlabel='Time', ylabel='Difference\nSimulation - Real')
     ax1.plot(time, differencies)
-    pos = nx.spring_layout(G, center=[1, 0], scale=1)
-    ax2 = plt.subplot(gs[1, :])  # row 1, span all columns
-    nx.draw_networkx_edge_labels(G, ax=ax2, pos=pos, edge_labels=edge_labels, font_color='red')
-    nx.draw_networkx(G, ax=ax2, node_size=1000, pos=pos,node_color="skyblue")
+
+    ax2 = plt.subplot(gs[0, 2])  # row 0, col 2
+    ax2.set(ylabel='Histogram filter normalized adjacencies')
+    edge_labels_ordered = OrderedDict(sorted(edge_labels.items(), key=lambda x: x[1], reverse=True))
+    dict_hist = dict_hist(config)
+    objects = []
+    performance = []
+    for k in edge_labels_ordered.keys():
+        objects.append(dict_hist[k[0]] + "-" + dict_hist[k[1]])
+    for v in edge_labels_ordered.values():
+        performance.append(float(v))
+    bar_color = color_bar(open_json(config["info"]["adj_file"]), edge_labels_ordered)
+    y_pos = np.arange(len(edge_labels_ordered.keys()))
+    plt.bar(y_pos, performance, align='center', alpha=0.5, color=bar_color)
+    plt.xticks(y_pos, objects, rotation=90)
+
+    pos = nx.circular_layout(G, center=[1, 0], scale=1)  # graph
+    ax3 = plt.subplot(gs[1, :])  # row 1, span all columns
+    nx.draw_networkx_edge_labels(G, ax=ax3, pos=pos, edge_labels=edge_labels, font_color='red')
+    nx.draw_networkx(G, ax=ax3, node_size=1000, pos=pos, node_color="skyblue")
     integral_text = ax1.text(0, -0.5, 10, bbox={'facecolor': 'yellow', 'alpha': 0.7, 'pad': 10})
     integral_text.set_text("Integral: " + str(round(integral)))
+    plt.tight_layout()
     plt.show()
-    fig.savefig(config_error["info"]["input_file_path"] + config_error["info"]["img_evaluation"])
+    fig.savefig(config_error["info"]["input_file_path"] + "/Evaluate_images/" + config_error["info"]["img_evaluation"])
--- HistogramFilter/Main.py
+++ HistogramFilter/Main.py
@@ -14,26 +14,29 @@ def open_json(conf):
 
 
 def system_set_up(data_config):
-    rf = ReadFile.ReadFile(data_config["info"]["input_file_path"]+data_config["info"]["input_file_name"]) #legge HF_input
+    rf = ReadFile.ReadFile(
+        data_config["info"]["input_file_path"] + data_config["info"]["input_file_name"])  # legge HF_input
     bel = data_config["probability"]["bel_t0"]  # probabilita di essere in quella stanza all inizio
-    pos = data_config["info"]["state_domain"]   # sono le iniziali delle stanze
+    pos = data_config["info"]["state_domain"]  # sono le iniziali delle stanze
     prob_state = []
     prefix = "prob"
     for i in pos:
-        prob_state.append(data_config["probability"][prefix+i])   #probabilita di quale stanza si va dalla stanza in cui si è
+        prob_state.append(
+            data_config["probability"][prefix + i])  # probabilita di quale stanza si va dalla stanza in cui si è
     prefix = "s"
     ser = []
     movement_transaction = data_config["info"]["movement_transaction"]
     for i in pos:
-        ser.append(data_config["sensor_error_probability"][prefix+i]) # dopo la misura di un sensore probabilita di essere in quella stanza dove è avvenuto il cambiamento
+        ser.append(data_config["sensor_error_probability"][
+                       prefix + i])  # dopo la misura di un sensore probabilita di essere in quella stanza dove è avvenuto il cambiamento
     belief = Belief.Belief(bel, pos, prob_state, ser, movement_transaction)
     return belief, rf
 
 
 def check_measure(new_measure, previous_measure):
-    out_sum = sum([x - y for x, y in zip(new_measure, previous_measure)])   #con zip faccio una tupla
+    out_sum = sum([x - y for x, y in zip(new_measure, previous_measure)])  # con zip faccio una tupla
     if out_sum == 0:
-        return {}   #se dalla misura precedente non cambia nulla non ritornare nulla in transactions
+        return {}  # se dalla misura precedente non cambia nulla non ritornare nulla in transactions
 
     new_measure_str = [str(int(x)) for x in new_measure]
     previous_measure_str = [str(int(x)) for x in previous_measure]
@@ -42,29 +45,32 @@ def check_measure(new_measure, previous_measure):
 
 
 def crate_file_output(df1: pd.DataFrame, df2, data_config):
-    df3 = ReadFile.ReadFile(data_config["info"]["input_file_path"]+
-                            data_config["info"]["ground_truth_file_name"]).df  # questo df3 è out.csv che rappresenta dove la persona è
+    df3 = ReadFile.ReadFile(data_config["info"]["input_file_path"] +
+                            data_config["info"][
+                                "ground_truth_file_name"]).df  # questo df3 è out.csv che rappresenta dove la persona è
     df1["Room"] = ""  # df1 è HF_input e df2 sono le probabilità
 
     for i, row in df1.copy().iterrows():
         cond = df3['Time'] <= row["Time"]
-        r = df3[cond].tail(1)['Room'].tolist()[0]  # prendo la riga in questione del ciclo e da li trovo la stanza da aggiungere
+        r = df3[cond].tail(1)['Room'].tolist()[
+            0]  # prendo la riga in questione del ciclo e da li trovo la stanza da aggiungere
         df1.loc[i, 'Room'] = r  # aggiungo le stanze in cui la persona si trova realmente da out.csv
 
     df = pd.merge(df1, df2, how='inner')
-    df.to_csv(data_config["info"]["input_file_path"]+data_config["info"]["output_file_name"], index=False)
+    df.to_csv(data_config["info"]["input_file_path"] + "/HF_out/" + data_config["info"]["output_file_name"],
+              index=False)
 
 
 if __name__ == "__main__":
-    #if len(sys.argv) < 2:
+    # if len(sys.argv) < 2:
     #    print("Manca il nome del file json")
     #    sys.exit(1)
-
-    conf_file = "config_error.json"
+    folder = "Configurations_file/"
+    conf_file = folder + "config_house1.json"
     config = open_json(conf_file)
 
-    belief, rf = system_set_up(config) # carica e inizializza
-    data_in = rf.df  #carico HF_input
+    belief, rf = system_set_up(config)  # carica e inizializza
+    data_in = rf.df  # carico HF_input
     columns = config["info"]["columns_name"]
     df = pd.DataFrame(columns=columns)
     i = 0
@@ -79,7 +85,7 @@ if __name__ == "__main__":
         if len(transactions) > 0:  # se qualche sensore è cambiato
             belief.bel_projected_upgrade()
             belief.bel_upgrade(transactions)
-            
+
         tmp = {}
         values = [time] + belief.bel
 
@@ -88,9 +94,7 @@ if __name__ == "__main__":
 
         df = df.append(tmp, ignore_index=True)
 
-        sensor_measures_previous = sensor_measures   #assegno a previous la misura precedente dei sensori
+        sensor_measures_previous = sensor_measures  # assegno a previous la misura precedente dei sensori
         i += 1
 
     crate_file_output(data_in, df, config)
-
-
